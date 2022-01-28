c847811fbd02784585312bfb3b71b5347f116a6d
Maxim Lipatov <maximmalyshyn@gmail.com>
2022-01-28 16:57:01 +0200

    downgrade to 12

--- src/app/components/route-info/route-info.component.ts
+++ src/app/components/route-info/route-info.component.ts
@@ -1,14 +1,16 @@
-import { Component, HostBinding, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, OnInit } from '@angular/core';
 import { MapStore } from '../map/map.store';
 
 @Component({
   selector: 'bzv-route-info',
   templateUrl: './route-info.component.html',
-  styleUrls: ['./route-info.component.less']
+  styleUrls: ['./route-info.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RouteInfoComponent implements OnInit {
   @HostBinding('class') cssClass = 'bzv-route-info';
-  routeNames$ = this.store.routeNames$
+  routeNames$ = this.store.routeNames$;
+  routes$ = this.store.routes$;
 
   constructor(private store: MapStore) { }
 
